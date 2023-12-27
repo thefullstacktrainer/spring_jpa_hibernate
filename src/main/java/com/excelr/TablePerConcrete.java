8d26be985d0133c7f36eb05d60b9d8867dd2ecc8
Lakshmikant <fullstacktrainer4u@gmail.com>
2023-12-28 02:15:04 +0530

    Table per concrete

--- src/main/java/com/excelr/TablePerConcrete.java
+++ src/main/java/com/excelr/TablePerConcrete.java
@@ -11,6 +11,9 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import com.excelr.entity.Contract_Employee;
 import com.excelr.entity.Employee;
 import com.excelr.entity.Regular_Employee;
+import com.excelr.entity.e3.Contract_Employee3;
+import com.excelr.entity.e3.Employee3;
+import com.excelr.entity.e3.Regular_Employee3;
 
 public class TablePerConcrete {
 	public static void main(String[] args) {
@@ -23,15 +26,15 @@ public class TablePerConcrete {
 
 		Transaction t = session.beginTransaction();
 
-		Employee e1 = new Employee();
+		Employee3 e1 = new Employee3();
 		e1.setName("Gaurav Chawla");
 
-		Regular_Employee e2 = new Regular_Employee();
+		Regular_Employee3 e2 = new Regular_Employee3();
 		e2.setName("Vivek Kumar");
 		e2.setSalary(50000);
 		e2.setBonus(5);
 
-		Contract_Employee e3 = new Contract_Employee();
+		Contract_Employee3 e3 = new Contract_Employee3();
 		e3.setName("Arjun Kumar");
 		e3.setPay_per_hour(1000);
 		e3.setContract_duration("15 hours");
---
+++ src/main/java/com/excelr/entity/e3/Contract_Employee3.java
@@ -0,0 +1,32 @@
+package com.excelr.entity.e3;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contractemployee102")
+@AttributeOverrides({ @AttributeOverride(name = "id", column = @Column(name = "id")),
+		@AttributeOverride(name = "name", column = @Column(name = "name")) })
+public class Contract_Employee3 extends Employee3 {
+
+	@Column(name = "pay_per_hour")
+	private float pay_per_hour;
+
+	@Column(name = "contract_duration")
+	private String contract_duration;
+
+	public float getPay_per_hour() {
+		return pay_per_hour;
+	}
+
+	public void setPay_per_hour(float payPerHour) {
+		pay_per_hour = payPerHour;
+	}
+
+	public String getContract_duration() {
+		return contract_duration;
+	}
+
+	public void setContract_duration(String contractDuration) {
+		contract_duration = contractDuration;
+	}
+}
